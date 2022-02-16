18aec2814577c6f30b54150d72b0f25a2058531d
mohammedgehad <pu2k@hotmail.com>
2022-02-16 22:00:05 +0530

    create a hook

---
+++ hooks/useThreejs.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+
+export const useRunThreejs = ({ selector, ThreeClass }) => {
+  useEffect(() => {
+    let el;
+    if (selector) {
+      el = document.querySelector(selector);
+    } else {
+      el = document.createElement("div");
+      el.style.height = "100vh";
+      document.body.appendChild(el);
+    }
+    new ThreeClass({ el });
+  }, []);
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRunThreejs } from "../threejs/ThreeApp";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
