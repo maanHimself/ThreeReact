551ff09c5e32124b246f2fe75fa439b5b58d99a9
maanHimself <64527563+maanHimself@users.noreply.github.com>
2022-02-16 23:25:01 +0530

    Update useThree.js

--- hooks/useThree.js
+++ hooks/useThree.js
@@ -2,7 +2,6 @@ import { useEffect, useRef } from "react";
 
 export const useThree = (ThreeClass) => {
   const canvas = useRef(null);
-  const el = null;
   useEffect(() => {
     new ThreeClass(canvas.current);
   }, []);
